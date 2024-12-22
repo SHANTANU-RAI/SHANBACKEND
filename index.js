f9e5ed2b8127bed61284a47ff7cae2171c613cb0
SHANTANU-RAI <raishantanu05@gmail.com>
2024-12-22 23:53:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req , res) => {
+    res.send('shan.com')
+})
+
+app.get('/login', (req,res) => {
+    res.send('<h1> heloo pls login </h1>')
+})
+
+app.get('/youtube' , (req, res) => {
+    res.send("<h1> shantanu rai </h1>")
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
